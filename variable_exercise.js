a64e581c28593a5ac38d18a8eb2cc0f47c915283
Tashmax19 <tashma19@gmail.com>
2021-03-12 10:29:50 -0800

    first commit

---
+++ variable_exercise.js
@@ -0,0 +1,22 @@
+//Declare four string variables.
+var myName="Mercy,";
+var var1="How";
+var var2="are";
+var var3="you?";
+
+
+//Declare four string variables.
+var myNum=1000;
+var num1=900;
+var num2=90;
+var num3=4;
+
+//Concatenate string variables.
+var stringConcat= myName +" " + var1+ " " + var2 +" "+ var3;
+
+//Concatenate number variables.
+var numConcat= myNum+num1+num2+num3;
+
+//Display concatenated number and string variables.
+console.log(stringConcat);
+console.log(numConcat);
---
+++ year.js
@@ -0,0 +1,8 @@
+var age=prompt("Please, state your age?");
+if(age>18){
+    alert("you are free to enjoy");
+}else if(age<18){
+    alert("Not sold to persons under the age of 18");
+}else{
+    alert("Age is not defined")
+}
